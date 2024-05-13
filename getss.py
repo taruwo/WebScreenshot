94cfd4514d33c28db051e05bbd6c939244376722
taruwo <44357751+taruwo@users.noreply.github.com>
2024-05-13 13:33:32 +0900

    ファイル名をURLから取るのではなくスクリーンショットのハッシュ値から取得するように変更

--- getss.py
+++ getss.py
@@ -1,21 +1,35 @@
 import asyncio
 from playwright.async_api import async_playwright
 import sys
-import urllib
+from urllib.parse import urlparse
 import os
+import hashlib
 
 urlname = sys.argv[1]
 
-filename = urllib.parse.quote(urlname, '')
+#basename = urllib.parse.quote(urlname, '')
+basename = urlparse(urlname).netloc
+filaname = basename + ".png"
+tmp_output_path = "/screenshot/" + filaname
 
-savepath = "/screenshot/" + filename + ".png"
+def rename_with_hash(filepath: str) -> str:
+    with open(filepath, 'rb') as file:
+        fdata = file.read()
+        hash_sha256 = hashlib.sha256(fdata).hexdigest()
+    hashed_filepath = "/screenshot/" + basename + "-" + hash_sha256 + ".png"
+    return hashed_filepath
 
 async def main():
     async with async_playwright() as p:
         browser = await p.chromium.launch(headless=True)
         page = await browser.new_page()
         await page.goto(urlname)
-        await page.screenshot(path=savepath, full_page=True)
-        os.chmod(savepath,0o666)
+        #await page.screenshot(path=savepath, full_page=True)
+        await page.screenshot(path=tmp_output_path)
+        output_path = rename_with_hash(tmp_output_path)
+        os.rename(tmp_output_path, output_path)
+        os.chmod(output_path,0o666)
+        filename = os.path.basename(output_path)
+        print(filename)
 
 asyncio.run(main())
